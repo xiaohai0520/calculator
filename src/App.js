1c8e570ba7d7cfa99ee8777cdf0be74f7bda3447
xiaohai sun <xs6925@g.rit.edu>
2021-07-25 01:21:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState} from 'react';
 
 function App() {
+
+  const [radius, setRadius] = useState('');
+  const [height, setHeight] = useState('');
+  const [density, setDensity] = useState('');
+  const [length, setLength] = useState('');
+  const [volume, setVolume] = useState('');
+  const [quality,setQuality] = useState('');
+  
+  const handleRadiusChange = (event) => {
+    setRadius(event.target.value);
+    cal();
+  }
+
+  const handleHeightChange = (event) => {
+     setHeight(event.target.value);
+     cal();
+  }
+
+  const handleLengthChange = (event) => {
+     setLength(event.target.value);
+     cal();
+  }
+
+  const handleDensityChange = (event) => {
+     setDensity(event.target.value);
+  }
+
+  const cal = () => {
+    const r = parseFloat(radius,10);
+    const h = parseFloat(height,10);
+    const l = parseFloat(length,10);
+    const d = parseFloat(density,10);
+    if (!isNaN(r) && !isNaN(h) && !isNaN(l) && !isNaN(d) && r > 0 && h > 0 && l > 0 && d > 0) {
+      const area = calArea(r,h);
+      console.log('area', area);
+      const total_v = area * l;
+      const total_quality = total_v * d;
+      setVolume(total_v);
+      setQuality(total_quality);
+    }
+  }
+
+  const calArea = (r, h) => {
+    const degree = Math.acos((r-h)/r);
+    const totalArea = r * r * degree * 2;
+    console.log('totalArea',totalArea);
+    const l = Math.tan(degree) * (r - h);
+    const triangleArea = l * (r - h);
+    return totalArea - triangleArea;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <label>
+      截面半径:
+        <textarea value={radius} onChange={handleRadiusChange} />
+      米
+      </label>
+
+      <br></br>
+
+      <label>
+      油面高度:
+        <textarea value={height} onChange={handleHeightChange } />
+      米
+      </label>
+
+      <br></br>
+
+      <label>
+      油管长度:
+        <textarea value={length} onChange={handleLengthChange } />
+      米
+      </label>
+
+      <br></br>
+
+      <label>
+      油密度:
+        <textarea value={density} onChange={handleDensityChange} />
+      吨/立方米
+      </label>
+
+      <br></br>
+      <button onClick={cal}>计算</button>
+      <br></br>
+
+      <label>
+      油体积:
+        <textarea value={volume} />
+      立方米
+      </label>
+
+      <br></br>
+
+      <label>
+      油量:
+        <textarea value={quality} />
+      吨
+      </label>
+        
       </header>
     </div>
   );
